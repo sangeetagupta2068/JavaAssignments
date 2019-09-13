607c4565b22201dfb1c07b595ae1eaf93b75da6e
sangeetagupta <tinnigupta1998@gmail.com>
2019-09-13 17:42:20 +0530

    fixed bugs for tsp

--- src/com/domain/sangeetagupta/algorithm_assignment/TravellingSalesmanImplementor.java
+++ src/com/domain/sangeetagupta/algorithm_assignment/TravellingSalesmanImplementor.java
@@ -4,88 +4,87 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.InputMismatchException;
-import java.util.Scanner;
-import java.util.Stack;
 
 public class TravellingSalesmanImplementor {
     private int nodeCount;
-    private Stack<Integer> integers;
     private int[][] adjacencyMatrix;
+    private boolean[] flag;
     private int[] visitedNodes;
+    private int sum;
 
-    public TravellingSalesmanImplementor(int nodeCount,String filePath) throws IOException{
+    public TravellingSalesmanImplementor(int nodeCount, String filePath) throws IOException {
         this.nodeCount = nodeCount;
         adjacencyMatrix = new int[nodeCount][nodeCount];
         initializeAdjacencyMatrix(filePath);
 
-        visitedNodes = new int[nodeCount+1];
-        integers = new Stack<>();
+        visitedNodes = new int[nodeCount];
+        flag = new boolean[nodeCount];
+        sum = 0;
 
     }
 
-    private void initializeAdjacencyMatrix(String filePath)throws IOException {
+    private void initializeAdjacencyMatrix(String filePath) throws IOException {
         FileReader fileReader = new FileReader(filePath);
         BufferedReader bufferedReader = new BufferedReader(fileReader);
         int count = 0;
         String distances;
 
-        while ((distances = bufferedReader.readLine())!=null){
+        while ((distances = bufferedReader.readLine()) != null) {
             String[] temp = distances.split("  ");
             adjacencyMatrix[count] = Arrays.stream(temp).mapToInt(Integer::valueOf).toArray();
-            count = count +1;
-        }
-
-        for(int row= 0; row<nodeCount; row++){
-            for (int column = 0; column<nodeCount; column++){
-                System.out.println(adjacencyMatrix[row][column]);
-            }
+            count = count + 1;
         }
     }
 
 
-    public void tsp()
-    {
-        visitedNodes[1] = 1;
-        integers.push(1);
-        int element, dst = 0, i;
-        int min = Integer.MAX_VALUE;
-        boolean minFlag = false;
-        System.out.print(1 + "\t");
-        while (!integers.isEmpty())
-        {
-            element = integers.peek();
-            i = 1;
-            min = Integer.MAX_VALUE;
-            while (i <= nodeCount)
-            {
-                if (adjacencyMatrix[element][i] > 1 && visitedNodes[i] == 0)
-                {
-                    if (min > adjacencyMatrix[element][i])
-                    {
-                        min = adjacencyMatrix[element][i];
-                        dst = i;
-                        minFlag = true;
-                    }
+    public void generateMinimumDistanceTour() {
+        int minimumDistance = Integer.MAX_VALUE;
+
+        int rowTemp = 0;
+        sum = 0;
+        int row = 0;
+        for (int count = 0; count < nodeCount; count++) {
+            minimumDistance = Integer.MAX_VALUE;
+            for (int column = 0; column < nodeCount; column++) {
+                if (column == row || flag[column]) {
+                    continue;
                 }
-                i++;
+                if (adjacencyMatrix[row][column] < minimumDistance) {
+                    minimumDistance = adjacencyMatrix[row][column];
+                    rowTemp = column;
+
+                }
             }
-            if (minFlag)
-            {
-                visitedNodes[dst] = 1;
-                integers.push(dst);
-                System.out.print(dst + "\t");
-                minFlag = false;
-                continue;
-            }
-            integers.pop();
+            visitedNodes[row] = rowTemp;
+            flag[rowTemp] = true;
+            row = rowTemp;
+            sum = sum + minimumDistance;
         }
+
+        sum = sum + adjacencyMatrix[row][0];
+
+        System.out.println(sum);
+
     }
 
-    public static void main(String... args){
+    public void displayPath() {
+        System.out.print((adjacencyMatrix[0][0]+1)+ "->");
+        for (int node : visitedNodes) {
+            System.out.print(node + 1 + "->");
+        }
+        System.out.print(adjacencyMatrix[0][0]+1 + "\n");
+    }
+
+    public void displayTotalMinimumDistance() {
+        System.out.println("Sum is: " + sum);
+    }
+
+    public static void main(String... args) {
         try {
-            TravellingSalesmanImplementor travellingSalesmanImplementor = new TravellingSalesmanImplementor(15,"/Users/sangeetagupta/IdeaProjects/JavaAssignments/src/com/domain/sangeetagupta/algorithm_assignment/tsp.txt");
-            travellingSalesmanImplementor.tsp();
+            TravellingSalesmanImplementor travellingSalesmanImplementor = new TravellingSalesmanImplementor(15, "/Users/sangeetagupta/IdeaProjects/JavaAssignments/src/com/domain/sangeetagupta/algorithm_assignment/tsp.txt");
+            travellingSalesmanImplementor.generateMinimumDistanceTour();
+            travellingSalesmanImplementor.displayPath();
+            travellingSalesmanImplementor.displayTotalMinimumDistance();
         } catch (IOException e) {
             e.printStackTrace();
         }
